88570599eb3981a3541be46cf1d8478e79a056a9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-12-14 21:43:49 +0100

    Extract functions from the evaluation code.

--- sethares_dissonance/sethares.py
+++ sethares_dissonance/sethares.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
-  
-def dissmeasure(fvec, amp, model='min'):
+
+def dissonance(fvec, amp, model='min'):
     """
     Given a list of partials in fvec, with amplitudes in amp, this routine
     calculates the dissonance by summing the roughness of every sine pair
@@ -13,22 +13,22 @@ def dissmeasure(fvec, amp, model='min'):
     """
     fvec = np.copy(fvec)
     amp = np.copy(amp)
- 
+
     # used to stretch dissonance curve for different freqs:
     Dstar = 0.24  # point of maximum dissonance
     S1 = 0.0207
     S2 = 18.96
- 
+
     C1 = 5
     C2 = -5
- 
+
     # Plomp-Levelt roughness curve:
     A1 = -3.51
     A2 = -5.75
- 
+
     ams = amp[np.argsort(fvec)]
     fvec = np.sort(fvec)
- 
+
     D = 0
     for i in range(1, len(fvec)):
         Fmin = fvec[:-i]
@@ -42,31 +42,33 @@ def dissmeasure(fvec, amp, model='min'):
             raise ValueError('model should be "min" or "product"')
         Dnew = a * (C1 * np.exp(A1 * S * Fdif) + C2 * np.exp(A2 * S * Fdif))
         D += np.sum(Dnew)
- 
-    return D
- 
- 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
 
-    # Some parameters from literature:
- 
+    return D
+
+def sethares_params():
     # Similar to Figure 3
     # http://sethares.engr.wisc.edu/consemi.html#anchor15619672
-    freqs = 500 * np.array([1, 2, 3, 4, 5, 6])
+    base_freq = 500
+    freqs = base_freq * np.array([1, 2, 3, 4, 5, 6])
     amps = 0.88 ** np.array([0, 1, 2, 3, 4, 5])
     alpharange = 2.3
     method = 'min'
- 
-#    # Davide Verotta Figure 4 example
-    # freqs = 261.63 * np.array([1, 2, 3, 4, 5, 6])
-    # amps = 1 / np.array([1, 2, 3, 4, 5, 6])
-    # alpharange = 2.0
-    # method = 'product'
+    return freqs, amps, alpharange, method
 
-    # Evaluate the dissonance function for each interval.
-    # Generate intervals by fixing one tone and varying the other.
-    interval_count = 1000
+def verotta_params():
+    # Davide Verotta - Figure 4 example
+    base_freq = 261.63
+    freqs = base_freq * np.array([1, 2, 3, 4, 5, 6])
+    amps = 1 / np.array([1, 2, 3, 4, 5, 6])
+    alpharange = 2.0
+    method = 'product'
+    return freqs, amps, alpharange, method
+
+def evaluate(freqs, amps, alpharange, method, interval_count=1000):
+    '''
+    Evaluate the dissonance function for each interval.
+    Generate intervals by fixing one tone and varying the other.
+    '''
     dissonances = np.zeros(interval_count)
     alphas = np.linspace(1, alpharange, interval_count)
     all_amps = np.hstack([amps, amps])
@@ -74,17 +76,19 @@ if __name__ == '__main__':
         other_freqs = alpha * freqs
         # concat frequencies from both tones in the interval
         all_freqs = np.hstack([freqs, other_freqs])
-        dissonances[i] = dissmeasure(all_freqs, all_amps, method)
+        dissonances[i] = dissonance(all_freqs, all_amps, method)
+    return dissonances, alphas
 
+def plot_dissonance_curve(dissonances, alphas):
     plt.plot(alphas, dissonances)
     plt.xscale('log')
-    plt.xlim(1, alpharange)
- 
+    plt.xlim(min(alphas), max(alphas))
+
     plt.xlabel('frequency ratio')
     plt.ylabel('dissonance')
 
     # annotate the simple ratios (just intervals in 12-TET)
-    # with vertical lines and labels 
+    # with vertical lines and labels
     for n, d in [(2,1), (3,2), (5,3), (4,3), (5,4), (6,5)]:
         x = n / d
         # position the label to some free space
@@ -92,6 +96,12 @@ if __name__ == '__main__':
         plt.axvline(x, color='silver')
         plt.annotate('%d:%d' % (n, d), (x, y),
                 horizontalalignment='center')
- 
-    plt.show()
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    dissonances, alphas = evaluate(*sethares_params())
+
+    plot_dissonance_curve(dissonances, alphas)
+    plt.show()    
     # plt.savefig('plot.png')
