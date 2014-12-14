75f2b917705f13244971010eb4867014c988851f
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-12-14 14:16:51 +0100

    Compute the amplitude envelope of a signal as the abs value of analytical signal via Hilbert transform.

--- generate_sound.py
+++ generate_sound.py
@@ -37,6 +37,9 @@ def generate_and_play(func, duration=1.):
     save_wav(samples, filename, normalize=True)
     play(filename)
 
+def amplitude_envelope(x):
+    return abs(hilbert(x))
+
 if __name__ == '__main__':
 
     # plain 440 Hz A for 1 second
@@ -70,3 +73,21 @@ if __name__ == '__main__':
     # constant 440 + raising ramp from 430 to 450
     generate_and_play(lambda t: sine(t, 440) + chirp(t, 440-10, 4, 440+10), duration=4)
     
+    # Dissonance of two sines f_1 and f_2 is like
+    # amplitude modulation of abs(f_1 - f_2) onto mean(f_1, f_2)
+    # The amplitude envelope can be obtained as the
+    # absolute value of the analytical signal (x + i * h(x))
+    # where h is the Hilbert transform.
+    # abs(scipy.signal.hilbert(x))
+    # It corresponds to the AM demodulation.
+    # The beating frequency is 2 * (f_1 - f_2), since its absolute
+    # value has twice higher the period.
+    t = sample_time(0, 1, 200)
+    x = sine(t, 10)  + sine(t, 12)
+    e = amplitude_envelope(x)
+    plot(t, x)
+    plot(t, e)
+    generate_and_play(lambda t: np.sum(sine(t, f) for f in (440, 445)), duration=3)
+
+    # derivative of the amplitude envelope
+    plot(t[:-1], abs(np.diff(abs(hilbert(x))))
