106075e0cc2938185f51bd0239778eb01278fdbd
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-12-14 20:52:45 +0100

    Use np package for numpy instead of direct imports.
    So that the usage of numpy function is clear.

--- sethares_dissonance/sethares.py
+++ sethares_dissonance/sethares.py
@@ -1,7 +1,6 @@
 from __future__ import division
-from numpy import exp, asarray, argsort, sort, sum, minimum
- 
- 
+import numpy as np
+  
 def dissmeasure(fvec, amp, model='min'):
     """
     Given a list of partials in fvec, with amplitudes in amp, this routine
@@ -12,8 +11,8 @@ def dissmeasure(fvec, amp, model='min'):
     amplitudes, but the newer model (model='min') is based on the minimum
     of the two amplitudes, since this matches the beat frequency amplitude.
     """
-    fvec = asarray(fvec)
-    amp = asarray(amp)
+    fvec = np.asarray(fvec)
+    amp = np.asarray(amp)
  
     # used to stretch dissonance curve for different freqs:
     Dstar = 0.24  # point of maximum dissonance
@@ -27,8 +26,8 @@ def dissmeasure(fvec, amp, model='min'):
     A1 = -3.51
     A2 = -5.75
  
-    ams = amp[argsort(fvec)]
-    fvec = sort(fvec)
+    ams = amp[np.argsort(fvec)]
+    fvec = np.sort(fvec)
  
     D = 0
     for i in range(1, len(fvec)):
@@ -36,43 +35,42 @@ def dissmeasure(fvec, amp, model='min'):
         S = Dstar / (S1 * Fmin + S2)
         Fdif = fvec[i:] - fvec[:-i]
         if model == 'min':
-            a = minimum(ams[:-i], ams[i:])
+            a = np.minimum(ams[:-i], ams[i:])
         elif model == 'product':
             a = ams[i:] * ams[:-i] # Older model
         else:
             raise ValueError('model should be "min" or "product"')
-        Dnew = a * (C1 * exp(A1 * S * Fdif) + C2 * exp(A2 * S * Fdif))
-        D += sum(Dnew)
+        Dnew = a * (C1 * np.exp(A1 * S * Fdif) + C2 * np.exp(A2 * S * Fdif))
+        D += np.sum(Dnew)
  
     return D
  
  
 if __name__ == '__main__':
-    from numpy import array, linspace, append
     import matplotlib.pyplot as plt
  
     # Similar to Figure 3
     # http://sethares.engr.wisc.edu/consemi.html#anchor15619672
-    freq = 500 * array([1, 2, 3, 4, 5, 6])
-    amp = 0.88**array([0, 1, 2, 3, 4, 5])
+    freq = 500 * np.array([1, 2, 3, 4, 5, 6])
+    amp = 0.88 ** np.array([0, 1, 2, 3, 4, 5])
     alpharange = 2.3
     method = 'min'
  
 #    # Davide Verotta Figure 4 example
-#    freq = 261.63 * array([1, 2, 3, 4, 5, 6])
-#    amp = 1 / array([1, 2, 3, 4, 5, 6])
-#    alpharange = 2.0
-#    method = 'product'
- 
+    # freq = 261.63 * np.array([1, 2, 3, 4, 5, 6])
+    # amp = 1 / np.array([1, 2, 3, 4, 5, 6])
+    # alpharange = 2.0
+    # method = 'product'
+
     # call function dissmeasure for each interval
-    diss = array([0])
-    for alpha in linspace(1, alpharange, 1000):
-        f = append(freq, alpha*freq)
-        a = append(amp, amp)
+    diss = np.array([0])
+    for alpha in np.linspace(1, alpharange, 1000):
+        f = np.append(freq, alpha*freq)
+        a = np.append(amp, amp)
         d = dissmeasure(f, a, method)
-        diss = append(diss, d)
+        diss = np.append(diss, d)
  
-    plt.plot(linspace(1, alpharange, len(diss)), diss)
+    plt.plot(np.linspace(1, alpharange, len(diss)), diss)
     plt.xscale('log')
     plt.xlim(1, alpharange)
  
@@ -86,3 +84,4 @@ if __name__ == '__main__':
                      horizontalalignment='center')
  
     plt.show()
+    # plt.savefig('plot.png')
