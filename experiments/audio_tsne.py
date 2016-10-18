311fa494bde023269f0675e3801e8749f21c877c
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-18 15:49:59 +0200

    Remove files unrelated to reassigned spectrograms.

--- experiments/audio_tsne.py
+++
@@ -1,6 +0,0 @@
-from sklearn.manifold import TSNE
-
-X_tsne = TSNE().fit_transform(X)
-
-colors=np.linspace(0,1,len(X))
-scatter(X_tsne[:,0], X_tsne[:,1], c=colors, alpha=0.1)
--- experiments/mix-audio-video.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -i piano-tsne.mp4 -i /Users/bzamecnik/Documents/harmoneye-labs/harmoneye/data/wav/c-scale-piano-mono.mp3 -map 0 -map 1 -codec copy -shortest piano-tsne-with-audio.mp4
