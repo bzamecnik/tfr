5d9686aeb3885a260998453d92283d46d26210c5
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-18 20:39:01 +0200

    Add a very basic setup.py file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tfr',
+      version='0.1',
+      description='Time-frequency reassigned spectrograms',
+      url='http://github.com/bzamecnik/tfr',
+      author='Bohumir Zamecnik',
+      author_email='bohumir.zamecnik@gmail.com',
+      license='MIT',
+      packages=['tfr'],
+      zip_safe=False)
