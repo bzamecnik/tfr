80398ad5e2f4d34d44c4c66a272edd8fcad2314f
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-20 14:12:18 +0200

    #1 Create a regression test on the several types of spectrogram computation.

---
+++ tests/regression_test.py
@@ -0,0 +1,24 @@
+"""
+A regression test for computing three kinds of spectrogram.
+
+Just to ensure we didn't break anything.
+"""
+
+import numpy as np
+import os
+from tfr.files import load_wav
+from tfr.spectrogram_features import spectrogram_features
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+def test_spectrograms():
+    for spectrogram_type in ['stft', 'reassigned', 'chromagram']:
+        yield assert_spectrogram_is_ok, spectrogram_type
+
+def assert_spectrogram_is_ok(spectrogram_type):
+    x, fs = load_wav(os.path.join(DATA_DIR, 'she_brings_to_me.wav'))
+    X = spectrogram_features(x, fs, block_size=4096, hop_size=2048,
+        spectrogram_type=spectrogram_type, to_log=True)
+    npz_file = os.path.join(DATA_DIR, 'she_brings_to_me_%s.npz' % spectrogram_type)
+    X_expected = np.load(npz_file)['arr_0']
+    assert np.allclose(X, X_expected)
