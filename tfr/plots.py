7c6f287da3579dd7488229d9d8917630558fade4
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-20 13:39:16 +0200

    Visualize a computed spectrogram from npz file to a png image.

--- tfr/plots.py
+++ tfr/plots.py
@@ -1,4 +1,6 @@
-import scipy
+import numpy as np
+import scipy.misc
+import sys
 
 
 def save_raw_spectrogram_bitmap(file_name, spectrogram):
@@ -9,3 +11,9 @@ def save_raw_spectrogram_bitmap(file_name, spectrogram):
     # rows = frequency bins (bottom to top)
     # columns = time (left to right)
     scipy.misc.imsave(file_name, spectrogram.T[::-1])
+
+def spectrogram_to_image(npz_file):
+  save_raw_spectrogram_bitmap(npz_file + '.png', np.load(npz_file)['arr_0'])
+
+if __name__ == '__main__':
+    spectrogram_to_image(sys.argv[1])
