63cc4b869ca5188f1f25e94f66fdc560c4f57008
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-26 00:13:11 +0200

    #16 Reassign only the positive half of the spectrum.
    This should exploit the symmetry of DFT on a real-valued signal
    in order to reduce computations.

--- tfr/reassignment.py
+++ tfr/reassignment.py
@@ -56,7 +56,7 @@ def estimate_group_delays(crossFreqSpectrum):
     "range: [-0.5, 0.5]"
     return 0.5 - arg(crossFreqSpectrum)
 
-def compute_spectra(x, w):
+def compute_spectra(x, w, positive_only=True):
     """
     This computes all the spectra needed for reassignment as well as estimates
     of instantaneous frequency and group delay.
@@ -75,23 +75,39 @@ def compute_spectra(x, w):
     # borders of the amplitude frame will go to zero anyway due to applying a
     # window function in the STFT tranform.
     X_prev_time = np.fft.fft(shift_right(x) * w)
+
     # spectrum shifted in frequency
     X_prev_freq = shift_right(X)
     X_cross_time = cross_spectrum(X, X_prev_time)
     X_cross_freq = cross_spectrum(X, X_prev_freq)
     X_inst_freqs = estimate_instant_freqs(X_cross_time)
     X_group_delays = estimate_group_delays(X_cross_freq)
+
+    if positive_only:
+        X_mag = positive_freq_magnitudes(X_mag)
+        X, X_cross_time, X_cross_freq, X_inst_freqs, X_group_delays = [
+            select_positive_freq_fft(values) for values in
+            [X, X_cross_time, X_cross_freq, X_inst_freqs, X_group_delays]
+        ]
+
     return X, X_mag, X_cross_time, X_cross_freq, X_inst_freqs, X_group_delays
 
-def requantize_f_spectrogram(X_mag, X_inst_freqs, to_log=True):
-    """Spectrogram requantized only in frequency"""
+def requantize_f_spectrogram(X_mag, X_inst_freqs, to_log=True, positive_only=True):
+    """
+    Spectrogram requantized only in frequency.
+
+    positive_only - indicates that input and output is the positive half of the
+    spectrum
+    """
     X_reassigned = np.empty(X_mag.shape)
     N = X_mag.shape[1]
+    # range of normalized frequencies
+    value_range = (0, 0.5) if positive_only else (0, 1)
     for i in range(X_mag.shape[0]):
-        X_reassigned[i, :] = np.histogram(X_inst_freqs[i], N, range=(0,1), weights=X_mag[i])[0]
+        X_reassigned[i, :] = np.histogram(X_inst_freqs[i], N, range=value_range, weights=X_mag[i])[0]
     X_reassigned = X_reassigned ** 2
     if to_log:
-         X_reassigned = db_scale(X_reassigned)
+        X_reassigned = db_scale(X_reassigned)
     return X_reassigned
 
 def requantize_tf_spectrogram(X_group_delays, X_inst_freqs, times, block_size, fs, weights=None):
@@ -125,9 +141,9 @@ def process_spectrogram(filename, block_size, hop_size):
     X_reassigned_tf = requantize_tf_spectrogram(X_group_delays, X_inst_freqs, times, block_size, fs, X_mag)[0]
     X_reassigned_tf = db_scale(X_reassigned_tf ** 2)
     image_filename = os.path.basename(filename).replace('.wav', '.png')
-    save_raw_spectrogram_bitmap('reassigned_f_' + image_filename, select_positive_freq_fft(X_reassigned_f))
-    save_raw_spectrogram_bitmap('reassigned_tf_' + image_filename, select_positive_freq_fft(X_reassigned_tf))
-    save_raw_spectrogram_bitmap('normal_' + image_filename, positive_freq_magnitudes(X_mag))
+    save_raw_spectrogram_bitmap('reassigned_f_' + image_filename, X_reassigned_f)
+    save_raw_spectrogram_bitmap('reassigned_tf_' + image_filename, X_reassigned_tf)
+    save_raw_spectrogram_bitmap('normal_' + image_filename, X_mag)
 
 #     X_time = X_group_delays + np.tile(np.arange(X.shape[0]).reshape(-1, 1), X.shape[1])
 #     idx = (abs(X).flatten() > 10) & (X_inst_freqs.flatten() < 0.5)
@@ -144,8 +160,7 @@ def reassigned_spectrogram(x, w, to_log=True):
     # TODO: The computed arrays are symetrical (positive vs. negative freqs).
     # We should only use one half.
     X, X_mag, X_cross_time, X_cross_freq, X_inst_freqs, X_group_delays = compute_spectra(x, w)
-    X_reassigned_f = requantize_f_spectrogram(X_mag, X_inst_freqs, to_log)
-    return select_positive_freq_fft(X_reassigned_f)
+    return requantize_f_spectrogram(X_mag, X_inst_freqs, to_log)
 
 def chromagram(x, w, fs, bin_range=(-48, 67), bin_division=1, to_log=True):
     """
@@ -155,14 +170,14 @@ def chromagram(x, w, fs, bin_range=(-48, 67), bin_division=1, to_log=True):
     # TODO: better give frequency range
     X, X_mag, X_cross_time, X_cross_freq, X_inst_freqs, X_group_delays = compute_spectra(x, w)
     n_blocks, n_freqs = X.shape
-    weights = positive_freq_magnitudes(X_mag).flatten()
+    weights = X_mag.flatten()
     eps = np.finfo(np.float32).eps
     pitch_quantizer = PitchQuantizer(Tuning(), bin_division=bin_division)
     # TODO: is it possible to quantize using relative freqs to avoid
     # dependency on the fs parameter?
-    pitch_bins = pitch_quantizer.quantize(np.maximum(fs * select_positive_freq_fft(X_inst_freqs), eps)).flatten()
+    pitch_bins = pitch_quantizer.quantize(np.maximum(fs * X_inst_freqs, eps)).flatten()
     X_chromagram = np.histogram2d(
-        np.repeat(np.arange(n_blocks), n_freqs / 2),
+        np.repeat(np.arange(n_blocks), n_freqs),
         pitch_bins,
         bins=(np.arange(n_blocks + 1),
               np.arange(bin_range[0], bin_range[1] + 1, 1 / bin_division)),
