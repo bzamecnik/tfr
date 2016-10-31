6d28f977de969286367be1ab108f04139bca1028
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-31 17:00:02 +0100

    Make an animation comparing the reassignment steps.

--- examples/multicomponent_spectrograms.py
+++ examples/multicomponent_spectrograms.py
@@ -6,6 +6,7 @@ much sharper altough it still cannot resolve inseparable places.
 
 import numpy as np
 import soundfile as sf
+import subprocess
 import tfr
 
 def sample_time(since, until, fs=44100.):
@@ -47,7 +48,7 @@ def generate_example_sound(fs=44100, duration=3, carrier_freq=2000, mod_freq=1, 
     sf.write('multicomponent.wav', x, fs)
     return x, fs
 
-if __name__ == '__main__':
+def compute_example_spectrograms():
     x, fs = generate_example_sound()
     signal_frames = tfr.SignalFrames(x, sample_rate=fs, frame_size=4096, hop_size=512)
 
@@ -57,28 +58,61 @@ if __name__ == '__main__':
     y_range = (84, 287) # 900-3100 Hz
 
     # plain non-reassigned spectrogram
-    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_stft.png',
-        band_pass(
-            tfr.reassigned_spectrogram(
-                signal_frames,
-                reassign_time=False,
-                reassign_frequency=False),
-            y_range))
+    X_stft = band_pass(
+        tfr.reassigned_spectrogram(
+            signal_frames,
+            reassign_time=False,
+            reassign_frequency=False),
+        y_range)
+    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_stft.png', X_stft)
 
     # time-reassigned spectrogram
-    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_t.png',
-        band_pass(
-            tfr.reassigned_spectrogram(signal_frames, reassign_frequency=False),
-            y_range))
+    X_t = band_pass(
+        tfr.reassigned_spectrogram(signal_frames, reassign_frequency=False),
+        y_range)
+    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_t.png', X_t)
 
     # frequency-reassigned spectrogram
-    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_f.png',
-        band_pass(
-            tfr.reassigned_spectrogram(signal_frames, reassign_time=False),
-            y_range))
+    X_f = band_pass(
+        tfr.reassigned_spectrogram(signal_frames, reassign_time=False),
+        y_range)
+    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_f.png', X_f)
 
     # time-frequency reassigned spectrogram
-    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_tf.png',
-        band_pass(
-            tfr.reassigned_spectrogram(signal_frames),
-            y_range))
+    X_tf = band_pass(tfr.reassigned_spectrogram(signal_frames), y_range)
+    tfr.plots.save_raw_spectrogram_bitmap('multicomponent_tf.png', X_tf)
+
+
+def make_animation():
+
+    def add_label_and_resize(source_file, dest_file, label):
+        subprocess.call([
+            'convert',
+            source_file,
+            # '-filter', 'point', '-resize', '200%',
+            # '-pointsize', '30',
+            'label:%s' % label, '+swap', '-gravity', 'Center', '-append',
+            dest_file])
+
+    for name, label in [('stft', 'no'), ('f', 'frequency'), ('t', 'time'), ('tf', 'time-frequency')]:
+        add_label_and_resize(
+            'multicomponent_%s.png' % name,
+            'multicomponent_%s_label.png' % name,
+            '%s reassignment' % label)
+
+    subprocess.call([
+        'convert',
+        '-delay', '100',
+        '-loop', '0',
+        'multicomponent_stft_label.png',
+        'multicomponent_f_label.png',
+        'multicomponent_tf_label.png',
+        'multicomponent_stft_label.png',
+        'multicomponent_t_label.png',
+        'multicomponent_tf_label.png',
+        'multicomponent_animation.gif'
+    ])
+
+if __name__ == '__main__':
+    compute_example_spectrograms()
+    make_animation()
