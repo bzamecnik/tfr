035ff2c50c5611406af172c6215f712086b75335
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-11-02 11:09:30 +0100

    Add the output_frame_size parameter to PitchgramTransformer.
    Without it the deserialization via jsonpickle fails.

--- tfr/sklearn.py
+++ tfr/sklearn.py
@@ -6,12 +6,13 @@ from .reassignment import pitchgram
 
 class PitchgramTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, sample_rate=44100, frame_size=4096, hop_size=2048,
+        output_frame_size=None,
         bin_range=[-48, 67], bin_division=1):
         self.sample_rate = sample_rate
         self.frame_size = frame_size
         self.hop_size = hop_size
-        # TODO: make this configurable
-        self.output_frame_size = hop_size
+        # if no output frame size is specified the input hop size is the default
+        self.output_frame_size = output_frame_size if output_frame_size is not None else hop_size
         self.bin_range = bin_range
         self.bin_division = bin_division
 
